1b6deda5e186e1843cc66705313d65d56c745750
PeZunino <pedrozsantos@hotmail.com>
2025-01-24 12:54:05 -0300

    feat(user): create "get user profile" service with tests and error handling

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,11 @@ import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository{
+	async findById(id: string): Promise<User | null> {
+		const user = await prisma.user.findUnique({where:{id}});
+
+		return user;	
+	}
 
 	async create(data:Prisma.UserCreateInput){
 		const user = await prisma.user.create({data});
